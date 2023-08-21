c81bb34ad2892b0eac8c7af4d89dc08b7eee14a7
hamihash <hami.hash@yahoo.com>
2023-08-21 23:12:46 +0330

    Debug

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,23 +10,41 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //        // creating object
+        //        let sphere = SCNSphere(radius: 0.2)
+        //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        //        let material = SCNMaterial() // setting material
+        //        material.diffuse.contents = UIImage(named: "art.scnassets/earth.jpg") // channging color
+        //        sphere.materials = [material] // setting the created material to cube
+        //
+        //
+        //
+        //        // craeting the space
+        //        let node = SCNNode()
+        //        node.position = SCNVector3(0, 0.1, -0.5) // setting its position
+        //        node.geometry = sphere // adding the created cube object to node
+        //
+        //        sceneView.scene.rootNode.addChildNode(node) // adding the created node to scene view's root node
         
+        sceneView.autoenablesDefaultLighting = true // enable default lighting to objects so its more realisitic
+
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+            let diceScene = SCNScene(named: "art.scnassets/dice.scn")!
+            // Node
+            let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true)!
+            diceNode.position = SCNVector3(0, 0, -0.1)
+
+            // Set the scene to the view
+            sceneView.scene.rootNode.addChildNode(diceNode)
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,41 +52,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        print(ARWorldTrackingConfiguration.isSupported)
+        
+        // this will enable horizental detection
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//
+//        // Pause the view's session
+//        sceneView.session.pause()
+//    }
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+    // Detected a horizontal space
+//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//        <#code#>
+//    }
 }
